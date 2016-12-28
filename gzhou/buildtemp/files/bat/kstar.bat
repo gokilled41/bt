55dbb9872ce9f00c8e54d310a0cd65dd24f8deae
Chudy <chudy0222@hotmail.com>
2016-12-28 21:37:04 +0800

    bat

--- gzhou/buildtemp/files/bat/kstar.bat
+++ gzhou/buildtemp/files/bat/kstar.bat
@@ -1,3 +1,3 @@
-call copy C:\Users\gzhou\Desktop\utils\chudy\typeandrun\Config.ini C:\Users\gzhou\Desktop\rename\Config-2.ini
-call copy D:\Workflow-G\software\typeandrun\Config.ini C:\Users\gzhou\Desktop\rename\Config.ini
-call ktar
+call al btg Config.ini/type acp rn ##Config-2.ini
+call al tar acp rn
+call ktar2
