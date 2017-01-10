730715e54e10f1f4bab4ca11a4fe8aa3b5adb8e2
Guifang Zhou <guifang03@126.com>
2017-01-10 16:51:14 +0800

    update

--- gzhou/buildtemp/files/bat/sadebug.bat
+++ gzhou/buildtemp/files/bat/sadebug.bat
@@ -7,18 +7,24 @@ if "%SA_TYPE%"=="OI7" goto OI7
 
 : OI6
 call al rb1 af dt_socket ar "'rem set'" "set"
+call al rb1 af dt_socket ar "'suspend=y'" "suspend=n"
 call al rb2 af dt_socket ar "'rem set'" "set"
 call al rb2 af dt_socket ar "'=8787'" "=18787"
+call al rb2 af dt_socket ar "'suspend=y'" "suspend=n"
 call al rb3 af dt_socket ar "'rem set'" "set"
 call al rb3 af dt_socket ar "'=8787'" "=28787"
+call al rb3 af dt_socket ar "'suspend=y'" "suspend=n"
 goto end
 
 : OI7
 call al jrb1 af dt_socket ar "'rem # set'" "set"
+call al jrb1 af dt_socket ar "'suspend=y'" "suspend=n"
 call al jrb2 af dt_socket ar "'rem # set'" "set"
 call al jrb2 af dt_socket ar "'=8787'" "=18787"
+call al jrb2 af dt_socket ar "'suspend=y'" "suspend=n"
 call al jrb3 af dt_socket ar "'rem # set'" "set"
 call al jrb3 af dt_socket ar "'=8787'" "=28787"
+call al jrb3 af dt_socket ar "'suspend=y'" "suspend=n"
 goto end
 
 : end
