8065461eb8a04b3d5365742cf48e50d8ac0c5ef4
Guifang Zhou <guifang03@126.com>
2017-01-10 14:26:29 +0800

    update

--- gzhou/buildtemp/files/bat/kgo.bat
+++ gzhou/buildtemp/files/bat/kgo.bat
@@ -1,7 +1,7 @@
+@echo off
 call btenv
 call btbak
 call go %btdir%
 call gadd
 call echo commting
-call git commit -a -m update > D:\alogs\gitcommit.log
-call git push > D:\alogs\gitpush.log
+call btcommit > D:\alogs\btcommit.log
