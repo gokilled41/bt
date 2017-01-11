6124a3ef4c6f45c5eb34943e7e2d4d041e68dd90
Guifang Zhou <guifang03@126.com>
2017-01-11 14:33:39 +0800

    update

--- gzhou/buildtemp/TempWmicBatchFile.bat
+++
@@ -1,6 +0,0 @@
-Path Win32_PerfFormattedData_Tcpip_NetworkInterface get BytesTotalPersec,CurrentBandwidth
-
-Process where name="esms.exe" get ExecutablePath,KernelModeTime,UserModeTime,WorkingSetSize
-
-QUIT
-
